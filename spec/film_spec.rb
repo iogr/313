b4fcf94cb2ab7e672e0c978968beafe1bc48732a
iogr <antgorbunov@gmail.com>
2020-11-26 21:22:51 +0300

    .new case film class test

--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -7,8 +7,15 @@ describe Film do
 
   describe '#to_s' do
     it 'returns human information about film' do
-      # "#{director} - #{title} (#{year})"
       expect(film.to_s).to eq "Christopher Nolan - Tenet (2020)"
+    end
+  end
+
+    describe '.new' do
+    it 'creates object with right attributes' do
+      expect(film.title).to eq 'Tenet'
+      expect(film.director).to eq 'Christopher Nolan'
+      expect(film.year).to eq '2020'
     end
   end
 end
